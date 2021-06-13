29f248cac7814d9a5eb966eb568782e0cfdfd9db
Андрей Беляков <ttxiom@gmail.com>
2021-06-13 19:14:44 +0500

    Update 24-2.py

--- py/day15/24-2.py
+++ py/day15/24-2.py
@@ -1,4 +1,20 @@
 f = open('24.txt')
 s = f.readline()
 f.close()
-print(max(map(len, s.split('XZZY'))))
+
+# s = 'XXXZZYYYZZXZXZXZZY'
+w = 'XZZY'
+
+cnt, max_cnt = 0, 0
+i = 0
+while i < len(s)-3:
+    if w !=  s[i:i+4]:
+        cnt += 1
+        i += 1
+    else:
+        if cnt > max_cnt:
+            max_cnt = cnt
+        cnt = 0
+        i += 4
+
+print(max_cnt)
